374ec36f0ca0c4150198901d29929cb1739742c6
Konrad Ciepielowski <ciepielowskikonrad@gmail.com>
2026-06-08 18:04:26 +0200

    feat: add UserRepository

---
+++ src/main/java/com/splitapp/splitApp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.splitapp.splitApp.repository;
+
+import com.splitapp.splitApp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
