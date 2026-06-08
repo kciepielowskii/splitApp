8427e70a3cfea1b9e470e20bb09e9c760c3c560f
Konrad Ciepielowski <ciepielowskikonrad@gmail.com>
2026-06-08 18:07:16 +0200

    feat: add UserDetailsServiceImpl and update User entity

--- src/main/java/com/splitapp/splitApp/model/User.java
+++ src/main/java/com/splitapp/splitApp/model/User.java
@@ -3,13 +3,16 @@ package com.splitapp.splitApp.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.time.LocalDateTime;
 import java.util.*;
 
 @Entity
 @Table(name = "users")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +33,34 @@ public class User {
 
     @ManyToMany(mappedBy = "members")
     private List<Group> groups = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
